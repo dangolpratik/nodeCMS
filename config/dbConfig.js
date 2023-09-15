3f436d3bc1ce484a76970ce6eb1276a8940fd668
Pratik Dangol <dangolpratik@gmail.com>
2023-09-15 21:42:25 +0545

    message

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,24 @@
-module.exports = {
-    HOST: "localhost",
+// //for localhost
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "root",
+//     PASSWORD: "",
+//     DB: "nodecms",
+//     dialect: "mysql",
+//     pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000,
+//     },
+//   };
+
+  //for live hosting
+  module.exports = {
+    HOST: "containers-us-west-184.railway.app",
     USER: "root",
-    PASSWORD: "",
-    DB: "nodecms",
+    PASSWORD: "xA70GKhDHh6yM35yDFXF",
+    DB: "railway",
     dialect: "mysql",
     pool: {
       max: 5,
